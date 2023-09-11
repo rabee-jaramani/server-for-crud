69c4e232f11a6f3b5c6c9a6d0ff53441208324a5
Rabee Aljaramani <rabeejermani@gmail.com>
2023-09-11 10:57:33 +0400

    test2

--- server.js
+++ server.js
@@ -5,19 +5,13 @@ const bodyParser = require('body-parser');
 const usersRouter = require('./routes/users');
 var cors = require('cors');
 const app = express();
-const multer = require('multer');
+// const multer = require('multer');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/users', usersRouter);
 const port = process.env.PORT || 5000;
-app.get('/', (req, res) => {
-  res.send('Hello, CRUD SERVERDKKKKKKKKKKDDD!');
-});
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 // mongoose.connect(
 //   'mongodb+srv://cruduser:cruduser123@clustercrud.ajooqoh.mongodb.net/?retryWrites=true&w=majority',
 //   {
@@ -31,19 +25,25 @@ app.listen(port, () => {
 //   console.log('Server is running and Connected to MongoDB');
 // });
 // Set up multer for file uploads
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Destination folder for storing uploaded files
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + '-' + uniqueSuffix + file.originalname); // File naming strategy
-  },
-});
-const upload = multer({ storage: storage });
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'uploads/'); // Destination folder for storing uploaded files
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+//     cb(null, file.fieldname + '-' + uniqueSuffix + file.originalname); // File naming strategy
+//   },
+// });
+// const upload = multer({ storage: storage });
 
 app.get('/users', async (req, res) => {
   let collection = await db.collection('users');
   let results = await collection.find({}).toArray();
   res.send(results).status(200);
+});
+app.get('/', (req, res) => {
+  res.send('Hello, CRUD SERVERDKKKKKKKKKKDDD!');
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
